ef0aaf86486734e07d90a71ebfde6772321adb1a
DeisyZermeno <deisy.zermeno23@gmail.com>
2024-11-14 19:17:55 -0600

    Pagina en la que se muestran las diferentes regiones del mundo de League of Legends direccionandote a la página de Riot.

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const links = document.querySelectorAll('nav ul li a');
+    links.forEach(link => {
+        link.addEventListener('click', (event) => {
+            event.preventDefault();
+            const target = document.querySelector(link.getAttribute('href'));
+            if (target) {
+                window.scrollTo({
+                    top: target.offsetTop - 50,
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
+});
